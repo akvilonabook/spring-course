34d949019d4040b44d15cd12b7ba133f5a71e029
Andrey Antonov <akvilona@list.ru>
2022-07-28 11:15:31 +0300

    Lesson 63

---
+++ Lesson63/src/main/java/ru/alishev/springcourse/services/ItemService.java
@@ -0,0 +1,34 @@
+/**
+ * Создал Андрей Антонов 27.07.2022 12:54
+ **/
+package ru.alishev.springcourse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.alishev.springcourse.models.Item;
+import ru.alishev.springcourse.models.Person;
+import ru.alishev.springcourse.repositories.ItemsRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class ItemService {
+    private final ItemsRepository itemsRepository;
+
+    @Autowired
+    public ItemService(ItemsRepository itemsRepository) {
+        this.itemsRepository = itemsRepository;
+    }
+
+    public List<Item> findByItemName(String itemName){
+        /** здесь обычно пишут бизнес логику, но у нас ее нет */
+         return itemsRepository.findByItemName(itemName);
+    }
+
+    public List<Item> findByOwner(Person owner) {
+        return itemsRepository.findByOwner(owner);
+    }
+
+}
